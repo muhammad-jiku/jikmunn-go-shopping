0a11e7b016a8fa028eb67c0cacd0cc9fe836f1ff
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-16 20:28:30 +0600

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  experimental: {
+    // appDir: true, // development
+    appDir: false, // production
+  },
+  images: {
+    unoptimized: true,
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: '**images.unsplash.com',
+    //   },
+    // ],
+  },
+};
+module.exports = nextConfig;
