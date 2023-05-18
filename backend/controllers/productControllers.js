d08521b1bef51910eadf21eb08749d7b2fc25d6a
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-18 11:48:04 +0600

    get single product func

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -7,6 +7,20 @@ export const getProducts = async (req, res, next) => {
   });
 };
 
+export const getProduct = async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    res.status(404).json({
+      error: 'Product not found.',
+    });
+  }
+
+  res.status(200).json({
+    product,
+  });
+};
+
 export const newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
