37cd6e918ef443dde39081a936522ff1f2790dab
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-18 13:44:48 +0600

    products displaying based on search, pagination filters

--- app/page.js
+++ app/page.js
@@ -1,19 +1,29 @@
- 
 import React from 'react';
 import axios from 'axios';
+import queryString from 'query-string';
 import { ListProducts } from '@/components';
 import dynamic from 'next/dynamic';
 
-const getProducts = async () => {
-  const { data } = await axios.get(`${process.env.BASE_URL}/api/products`);
+const getProducts = async (searchParams) => {
+  const urlParams = {
+    keyword: searchParams.keyword,
+    page: searchParams.page,
+  };
+
+  const searchQuery = queryString.stringify(urlParams);
+
+  const { data } = await axios.get(
+    `${process.env.BASE_URL}/api/products?${searchQuery}`
+  );
 
   return data;
 };
 
-const Home = async () => {
-  const productsData = await getProducts();
+const Home = async ({ searchParams }) => {
+  const productsData = await getProducts(searchParams);
 
   return <ListProducts data={productsData} />;
 };
 
-export default dynamic(() => Promise.resolve(Home), { ssr: false });
+export default Home;
+// export default dynamic(() => Promise.resolve(Home), { ssr: false });
