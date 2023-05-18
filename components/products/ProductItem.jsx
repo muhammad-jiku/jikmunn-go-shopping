4ac5fdde918bfd4066d850cba8f65265db7e90d9
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-18 22:30:09 +0600

    Product item component modfied

--- components/products/ProductItem.jsx
+++ components/products/ProductItem.jsx
@@ -1,12 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import StarRatings from 'react-star-ratings';
 import Image from 'next/image';
 import productImg from '../../assets/images/default_product.png';
+import CartContext from '@/context/CartContext';
 
 const ProductItem = ({ product }) => {
+  const { addItemToCart } = useContext(CartContext);
+
+  const addToCartHandler = () => {
+    addItemToCart({
+      product: product._id,
+      name: product.name,
+      price: product.price,
+      image: product.images[0].url,
+      stock: product.stock,
+      seller: product.seller,
+    });
+  };
+
   return (
     <article className="border border-gray-200 overflow-hidden bg-white shadow-sm rounded mb-5">
       <div className="flex flex-col md:flex-row">
@@ -63,9 +77,11 @@ const ProductItem = ({ product }) => {
 
             <p className="text-green-500">Free Shipping</p>
             <div className="my-3">
-              <a className="px-4 py-2 inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 cursor-pointer">
-                {' '}
-                Add to Cart{' '}
+              <a
+                className="px-4 py-2 inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 cursor-pointer"
+                onClick={addToCartHandler}
+              >
+                Add to Cart
               </a>
             </div>
           </div>
