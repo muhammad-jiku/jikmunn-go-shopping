f17574363995202a6548c9a375b8c326e3691a71
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-19 12:08:42 +0600

    sign up page

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as ListProducts } from './products/ListProducts';
 export { default as ProductDetails } from './products/ProductDetails';
 export { default as Cart } from './cart/Cart';
+export { default as SignUp } from './auth/SignUp';
