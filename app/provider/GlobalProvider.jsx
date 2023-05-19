eb93ed4cf63b7d4a7328c6ed70565d0cac021b57
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-19 11:21:36 +0600

    toastcontainer

--- app/provider/GlobalProvider.jsx
+++ app/provider/GlobalProvider.jsx
@@ -1,9 +1,11 @@
 import { AuthProvider } from '@/context/AuthContext';
 import { CartProvider } from '@/context/CartContext';
+import { ToastContainer } from 'react-toastify';
 
 export function GlobalProvider({ children }) {
   return (
     <>
+      <ToastContainer position="bottom-right" />
       <AuthProvider>
         <CartProvider>{children}</CartProvider>
       </AuthProvider>
