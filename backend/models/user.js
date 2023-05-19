b14687ccf62a7bfb3b387b42a2794e7ffa14b77c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-19 10:45:55 +0600

    user model

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter your email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your password'],
+      minLength: [6, 'Your password must be longer than 6 characters'],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: 'user',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.models.User || new mongoose.model('User', userSchema);
+
+export default User;
