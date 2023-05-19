abd4ebdfd2f2d3cd23d076ac1857b8e3002694e3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-19 20:01:37 +0600

    profile page

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as ProductDetails } from './products/ProductDetails';
 export { default as Cart } from './cart/Cart';
 export { default as SignUp } from './auth/SignUp';
 export { default as SignIn } from './auth/SignIn';
+export { default as Profile } from './auth/Profile';
