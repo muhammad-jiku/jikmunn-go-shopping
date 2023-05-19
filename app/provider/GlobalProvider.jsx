1e9b976740d9593c6c6365c5299eb9471ba9fed3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-19 13:02:08 +0600

    Session provider

--- app/provider/GlobalProvider.jsx
+++ app/provider/GlobalProvider.jsx
@@ -1,13 +1,17 @@
 import { AuthProvider } from '@/context/AuthContext';
 import { CartProvider } from '@/context/CartContext';
 import { ToastContainer } from 'react-toastify';
+import { SessionProvider } from 'next-auth/react';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function GlobalProvider({ children }) {
   return (
     <>
       <ToastContainer position="bottom-right" />
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        <CartProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </CartProvider>
       </AuthProvider>
     </>
   );
