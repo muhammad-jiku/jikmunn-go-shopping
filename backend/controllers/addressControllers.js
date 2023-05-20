1d570dcd97452da884e58455717660ba8af8c999
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-20 20:32:07 +0600

    get address func in controller

--- backend/controllers/addressControllers.js
+++ backend/controllers/addressControllers.js
@@ -1,4 +1,6 @@
+import next from 'next';
 import Address from '../models/address';
+import ErrorHandler from '../utils/ErrorHandler';
 
 export const newAddress = async (req, res) => {
   req.body.user = req.user._id;
@@ -15,5 +17,17 @@ export const getAddresses = async (req, res) => {
 
   res.status(200).json({
     addresses,
+  });
+};
+
+export const getAddress = async (req, res) => {
+  const address = await Address.findById(req.query.id);
+
+  if (!address) {
+    return next(new ErrorHandler('Address not found', 404));
+  }
+
+  res.status(200).json({
+    address,
   });
 };
