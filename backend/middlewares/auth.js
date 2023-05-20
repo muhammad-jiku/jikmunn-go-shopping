4c99e439e636d1485df70615c66268f81421e302
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-20 19:08:19 +0600

    auth handling middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,14 @@
+import { getSession } from 'next-auth/react';
+import ErrorHandler from '../utils/ErrorHandler';
+
+export const isAuthenticatedUser = async (req, res, next) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return next(new ErrorHandler('Login first to access this route', 401));
+  }
+
+  req.user = session.user;
+
+  next();
+};
