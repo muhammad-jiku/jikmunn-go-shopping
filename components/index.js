50ffda7d94275ca0a965bf1897a697d99bce822c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-20 13:55:23 +0600

    New Address Page

--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Cart } from './cart/Cart';
 export { default as SignUp } from './auth/SignUp';
 export { default as SignIn } from './auth/SignIn';
 export { default as Profile } from './auth/Profile';
+export { default as NewAddress } from './user/NewAddress';
