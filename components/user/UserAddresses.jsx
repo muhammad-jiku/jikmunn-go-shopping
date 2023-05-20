ff4c8524f54cdab553c7cacd222c686477e77059
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-20 12:46:47 +0600

    user addresses component

---
+++ components/user/UserAddresses.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { BiMap } from 'react-icons/bi';
+
+const UserAddresses = ({ addresses }) => {
+  return (
+    <>
+      {addresses?.map((address) => (
+        <Link href={`/address/${address?._id}`} key={address?._id}>
+          <div className="mb-5 gap-4">
+            <figure className="w-full flex align-center bg-gray-100 p-4 rounded-md cursor-pointer">
+              <div className="mr-3">
+                <span className="flex items-center justify-center text-yellow-500 w-12 h-12 bg-white rounded-full shadow mt-2">
+                  <BiMap />
+                </span>
+              </div>
+              <figcaption className="text-gray-600">
+                <p>
+                  {address?.street} <br /> {address?.city}, {address?.state},{' '}
+                  {address?.zipCode}, {address?.country}
+                  <br />
+                  Phone no: {address?.phoneNo}
+                </p>
+              </figcaption>
+            </figure>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default UserAddresses;
