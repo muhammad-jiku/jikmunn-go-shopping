2b1c3c9fc4b63fcd305d0fd857c58a12af4839e1
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-20 20:28:18 +0600

    address controller modifed

--- backend/controllers/addressControllers.js
+++ backend/controllers/addressControllers.js
@@ -1,6 +1,8 @@
 import Address from '../models/address';
 
 export const newAddress = async (req, res) => {
+  req.body.user = req.user._id;
+
   const address = await Address.create(req.body);
 
   res.status(200).json({
@@ -9,7 +11,7 @@ export const newAddress = async (req, res) => {
 };
 
 export const getAddresses = async (req, res) => {
-  const addresses = await Address.find();
+  const addresses = await Address.find({ user: req.user._id });
 
   res.status(200).json({
     addresses,
