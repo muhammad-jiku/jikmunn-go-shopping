996927c0791deb75cd121bafa3fc0d9acd3a3f49
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-03 12:55:21 +0600

    config modification

--- next.config.js
+++ next.config.js
@@ -4,20 +4,24 @@ const nextConfig = {
 		appDir: true, // development
 		// appDir: false, // production
 	},
-	env: {
-		BASE_URL: `${process.env.BASE_URL}`,
-		DB_URI: `${process.env.DB_URI}`,
-		NEXTAUTH_URL: `${process.env.NEXTAUTH_URL}`,
-		NEXTAUTH_SECRET: `${process.env.NEXTAUTH_SECRET}`,
-		CLOUDINARY_NAME: `${process.env.CLOUDINARY_NAME}`,
-		CLOUDINARY_API_KEY: `${process.env.CLOUDINARY_API_KEY}`,
-		CLOUDINARY_API_SECRET: `${process.env.CLOUDINARY_API_SECRET}`,
-		CLOUDINARY_API_URL: `${process.env.CLOUDINARY_API_URL}`,
-	},
+	// env: {
+	// 	BASE_URL: `${process.env.BASE_URL}`,
+	// 	DB_URI: `${process.env.DB_URI}`,
+	// 	NEXTAUTH_URL: `${process.env.NEXTAUTH_URL}`,
+	// 	NEXTAUTH_SECRET: `${process.env.NEXTAUTH_SECRET}`,
+	// 	CLOUDINARY_NAME: `${process.env.CLOUDINARY_NAME}`,
+	// 	CLOUDINARY_API_KEY: `${process.env.CLOUDINARY_API_KEY}`,
+	// 	CLOUDINARY_API_SECRET: `${process.env.CLOUDINARY_API_SECRET}`,
+	// 	CLOUDINARY_API_URL: `${process.env.CLOUDINARY_API_URL}`,
+	// },
 	reactStrictMode: true,
 	images: {
 		unoptimized: true,
 		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**lh3.googleusercontent.com',
+			},
 			{
 				protocol: 'https',
 				hostname: '**res.cloudinary.com',
@@ -25,4 +29,5 @@ const nextConfig = {
 		],
 	},
 };
+
 module.exports = nextConfig;
