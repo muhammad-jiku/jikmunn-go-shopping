23ab3168f3fe9a394853ac1e0de0f381cbf88435
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-04 18:00:48 +0600

    drop down component

---
+++ components/shared/DropDownLink.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+
+export default function DropDownLink(props) {
+	let { href, children, ...rest } = props;
+	return (
+		<Link
+			href={href}
+			{...rest}
+		>
+			{children}
+		</Link>
+	);
+}
