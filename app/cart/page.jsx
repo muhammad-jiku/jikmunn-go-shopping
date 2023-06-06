6106c2228f5d5a50f63d744dbfc9e8dcd8dc2dd3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-07 01:48:09 +0600

    cart page

---
+++ app/cart/page.jsx
@@ -0,0 +1,8 @@
+import { Cart } from '@/components';
+import React from 'react';
+
+const Page = () => {
+  return <Cart />;
+};
+
+export default Page;
--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './shared/Navbar';
 export { default as ProductLists } from './products/ProductLists';
 export { default as ProductDetails } from './products/ProductDetails';
+export { default as Cart } from './cart/Cart';
