2b2ca66c16e87a3f56717432aa398fa0c9f409fd
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-06 18:50:37 +0600

    get product by id controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,3 +35,27 @@ export const getProducts = async (req, res, next) => {
 		});
 	}
 };
+
+export const getProduct = async (req, res, next) => {
+	try {
+		const product = await Product.findById({ _id: req.query.id });
+
+		if (!product) {
+			res.status(404).json({
+				error: 'Something went missing!',
+			});
+		}
+
+		return res.status(200).json({
+			success: true,
+			data: product,
+			message: 'product displayed successfully!',
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			success: false,
+			message: 'Something went wrong',
+		});
+	}
+};
