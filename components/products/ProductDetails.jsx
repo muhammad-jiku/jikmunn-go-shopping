f407297e2a1a3b89a88bedae6483dd86ca996c83
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-07 01:33:02 +0600

    cart context applied to the component

--- components/products/ProductDetails.jsx
+++ components/products/ProductDetails.jsx
@@ -1,173 +1,182 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import StarRatings from 'react-star-ratings';
 import { BsCart } from 'react-icons/bs';
 import BreadCrumbs from './BreadCrumbs';
 import axios from 'axios';
 import productImg from '../../assets/images/default_product.png';
+import CartContext from '@/context/CartContext';
 
 const ProductDetails = ({ params }) => {
-	const [productData, setProductData] = useState([]);
+  const { addItemToCart } = useContext(CartContext);
+  const [productData, setProductData] = useState([]);
 
-	const getProductDetails = async (id) => {
-		// const { data } = await axios.get(`${process.env.API_URL}/api/v1/products/${id}`);
-		const { data } = await axios.get(`/api/v1/products/${id}`);
-		// console.log('data', data?.data);
-		setProductData(data?.data);
-		return data?.data;
-	};
+  const getProductDetails = async (id) => {
+    // const { data } = await axios.get(`${process.env.API_URL}/api/v1/products/${id}`);
+    const { data } = await axios.get(`/api/v1/products/${id}`);
+    // console.log('data', data?.data);
+    setProductData(data?.data);
+    return data?.data;
+  };
 
-	useEffect(() => {
-		getProductDetails(params?.id);
-	}, [params?.id]);
-	// console.log('product data', productData);
+  useEffect(() => {
+    getProductDetails(params?.id);
+  }, [params?.id]);
+  // console.log('product data', productData);
 
-	const imgRef = useRef(null);
+  const imgRef = useRef(null);
 
-	const setImgPreview = (url) => {
-		imgRef.current.src = url;
-	};
+  const setImgPreview = (url) => {
+    imgRef.current.src = url;
+  };
 
-	const inStock = productData?.stock >= 10;
+  const inStock = productData?.stock >= 10;
 
-	const breadCrumbs = [
-		{ name: 'Home', url: '/' },
-		{
-			name: `${productData?.name?.substring(0, 100)} ...`,
-			url: `/products/${productData?._id}`,
-		},
-	];
+  const addToCartHandler = () => {
+    addItemToCart({
+      product: productData?._id,
+      name: productData?.name,
+      price: productData?.price,
+      image: productData?.images[0].url,
+      stock: productData?.stock,
+      seller: productData?.seller,
+    });
+  };
 
-	return (
-		<>
-			<BreadCrumbs breadCrumbs={breadCrumbs} />
-			<section className='bg-white py-10'>
-				<div className='container max-w-screen-xl mx-auto px-4'>
-					<div className='grid grid-cols-1 sm:grid-cols-2 gap-8 mb-5'>
-						<aside>
-							<div className='border border-gray-200 shadow-sm p-3 text-center rounded mb-5'>
-								<img
-									ref={imgRef}
-									className='object-cover inline-block'
-									src={
-										productData?.images?.[0]
-											? productData?.images?.[0]?.url
-											: productImg.src
-									}
-									alt='Product title'
-									width='340'
-									height='340'
-								/>
-							</div>
-							<div className='space-x-2 overflow-auto text-center whitespace-nowrap'>
-								{productData?.images?.map((img, index) => (
-									<a
-										key={index}
-										className='inline-block border border-gray-200 p-1 rounded-md hover:border-blue-500 cursor-pointer'
-										onClick={() => setImgPreview(img?.url)}
-									>
-										<img
-											className='w-14 h-14'
-											src={img.url}
-											alt='Product title'
-											width='500'
-											height='500'
-										/>
-									</a>
-								))}
-							</div>
-						</aside>
-						<main>
-							<h2 className='font-semibold text-2xl mb-4'>
-								{productData?.name}
-							</h2>
+  const breadCrumbs = [
+    { name: 'Home', url: '/' },
+    {
+      name: `${productData?.name?.substring(0, 100)} ...`,
+      url: `/products/${productData?._id}`,
+    },
+  ];
 
-							<div className='flex flex-wrap items-center space-x-2 mb-2'>
-								<div className='ratings'>
-									<StarRatings
-										rating={productData?.ratings}
-										starRatedColor='#ffb829'
-										numberOfStars={5}
-										starDimension='20px'
-										starSpacing='2px'
-										name='rating'
-									/>
-								</div>
-								<span className='text-yellow-500'>{productData?.ratings}</span>
+  return (
+    <>
+      <BreadCrumbs breadCrumbs={breadCrumbs} />
+      <section className='bg-white py-10'>
+        <div className='container max-w-screen-xl mx-auto px-4'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 mb-5'>
+            <aside>
+              <div className='border border-gray-200 shadow-sm p-3 text-center rounded mb-5'>
+                <img
+                  ref={imgRef}
+                  className='object-cover inline-block'
+                  src={
+                    productData?.images?.[0]
+                      ? productData?.images?.[0]?.url
+                      : productImg.src
+                  }
+                  alt='Product title'
+                  width='340'
+                  height='340'
+                />
+              </div>
+              <div className='space-x-2 overflow-auto text-center whitespace-nowrap'>
+                {productData?.images?.map((img, index) => (
+                  <a
+                    key={index}
+                    className='inline-block border border-gray-200 p-1 rounded-md hover:border-blue-500 cursor-pointer'
+                    onClick={() => setImgPreview(img?.url)}
+                  >
+                    <img
+                      className='w-14 h-14'
+                      src={img.url}
+                      alt='Product title'
+                      width='500'
+                      height='500'
+                    />
+                  </a>
+                ))}
+              </div>
+            </aside>
+            <main>
+              <h2 className='font-semibold text-2xl mb-4'>
+                {productData?.name}
+              </h2>
 
-								<svg
-									width='6px'
-									height='6px'
-									viewBox='0 0 6 6'
-									xmlns='http://www.w3.org/2000/svg'
-								>
-									<circle
-										cx='3'
-										cy='3'
-										r='3'
-										fill='#DBDBDB'
-									/>
-								</svg>
+              <div className='flex flex-wrap items-center space-x-2 mb-2'>
+                <div className='ratings'>
+                  <StarRatings
+                    rating={productData?.ratings}
+                    starRatedColor='#ffb829'
+                    numberOfStars={5}
+                    starDimension='20px'
+                    starSpacing='2px'
+                    name='rating'
+                  />
+                </div>
+                <span className='text-yellow-500'>{productData?.ratings}</span>
 
-								<span className='text-green-500'>Verified</span>
-							</div>
+                <svg
+                  width='6px'
+                  height='6px'
+                  viewBox='0 0 6 6'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <circle cx='3' cy='3' r='3' fill='#DBDBDB' />
+                </svg>
 
-							<p className='mb-4 font-semibold text-xl'>
-								${productData?.price}
-							</p>
+                <span className='text-green-500'>Verified</span>
+              </div>
 
-							<p className='mb-4 text-gray-500'>{productData?.description}</p>
+              <p className='mb-4 font-semibold text-xl'>
+                ${productData?.price}
+              </p>
 
-							<div className='flex flex-wrap gap-2 mb-5'>
-								<button
-									className='px-4 py-2 flex justify-around items-center text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700'
-									disabled={inStock ? false : true}
-								>
-									<BsCart className='text-2xl' />
-									<span className='ml-1'> Add to cart</span>
-								</button>
-							</div>
+              <p className='mb-4 text-gray-500'>{productData?.description}</p>
 
-							<ul className='mb-5'>
-								<li className='mb-1'>
-									{' '}
-									<b className='font-medium w-36 inline-block'>Stock</b>
-									{inStock ? (
-										<span className='text-green-500'>In Stock</span>
-									) : (
-										<span className='text-red-500'>Out of Stock</span>
-									)}
-								</li>
-								<li className='mb-1'>
-									{' '}
-									<b className='font-medium w-36 inline-block'>Category:</b>
-									<span className='text-gray-500'>{productData?.category}</span>
-								</li>
-								<li className='mb-1'>
-									{' '}
-									<b className='font-medium w-36 inline-block'>
-										Seller / Brand:
-									</b>
-									<span className='text-gray-500'>{productData?.seller}</span>
-								</li>
-							</ul>
-						</main>
-					</div>
+              <div className='flex flex-wrap gap-2 mb-5'>
+                <button
+                  className='px-4 py-2 inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700'
+                  onClick={addToCartHandler}
+                  disabled={!inStock}
+                >
+                  <BsCart className='text-2xl' />
+                  <span className='ml-1'> Add to cart</span>
+                </button>
+              </div>
 
-					{/* <NewReview /> */}
-					<hr />
+              <ul className='mb-5'>
+                <li className='mb-1'>
+                  {' '}
+                  <b className='font-medium w-36 inline-block'>Stock</b>
+                  {inStock ? (
+                    <span className='text-green-500'>In Stock</span>
+                  ) : (
+                    <span className='text-red-500'>Out of Stock</span>
+                  )}
+                </li>
+                <li className='mb-1'>
+                  {' '}
+                  <b className='font-medium w-36 inline-block'>Category:</b>
+                  <span className='text-gray-500'>{productData?.category}</span>
+                </li>
+                <li className='mb-1'>
+                  {' '}
+                  <b className='font-medium w-36 inline-block'>
+                    Seller / Brand:
+                  </b>
+                  <span className='text-gray-500'>{productData?.seller}</span>
+                </li>
+              </ul>
+            </main>
+          </div>
 
-					<div className='font-semibold'>
-						<h1 className='text-gray-500 review-title mb-6 mt-10 text-2xl'>
-							Other Customers Reviews
-						</h1>
-						{/* <Reviews /> */}
-					</div>
-				</div>
-			</section>
-		</>
-	);
+          {/* <NewReview /> */}
+          <hr />
+
+          <div className='font-semibold'>
+            <h1 className='text-gray-500 review-title mb-6 mt-10 text-2xl'>
+              Other Customers Reviews
+            </h1>
+            {/* <Reviews /> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetails;
--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import logoImg from '../../assets/images/logo.png';
@@ -8,70 +8,75 @@ import profileImg from '../../assets/images/default_profile_avatar.png';
 import Search from './Search';
 import { BsCart, BsFillPersonFill } from 'react-icons/bs';
 import { FaBars } from 'react-icons/fa';
+import CartContext from '@/context/CartContext';
 
 const Navbar = () => {
-	return (
-		<header className='bg-white py-2 border-b'>
-			<div className='container max-w-screen-xl mx-auto px-4'>
-				<div className='flex flex-wrap items-center'>
-					<div className='flex-shrink-0 mr-5'>
-						<a href='/'>
-							<Image
-								src={logoImg.src}
-								height='40'
-								width='120'
-								alt='GoShopping'
-							/>
-						</a>
-					</div>
-					<Search />
+  const { cart } = useContext(CartContext);
+  const cartItems = cart?.cartItems;
 
-					<div className='flex items-center space-x-2 ml-auto'>
-						<Link
-							href='/cart'
-							className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
-						>
-							<BsCart className='inline lg:hidden ml-1' />
-							<span className='hidden lg:inline ml-1'>Cart</span> (<b>0</b>)
-						</Link>
-						<Link
-							href='/login'
-							className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
-						>
-							<BsFillPersonFill className='inline lg:hidden ml-1' />
-							<span className='hidden lg:inline ml-1'>Sign in</span>
-						</Link>
-						<Link href='/me'>
-							<div className='flex items-center mb-4 space-x-3 mt-4 cursor-pointer'>
-								<img
-									className='w-10 h-10 rounded-full'
-									src={profileImg.src}
-									loading='lazy'
-								/>
-								<div className='space-y-1 font-medium'>
-									<p>
-										testuser
-										<time className='block text-sm text-gray-500 dark:text-gray-400'>
-											test@gmail.com
-										</time>
-									</p>
-								</div>
-							</div>
-						</Link>
-					</div>
-					<div className='lg:hidden ml-2'>
-						<button
-							type='button'
-							className='bg-white p-3 inline-flex items-center rounded-md text-black hover:bg-gray-200 hover:text-gray-800 border border-transparent'
-						>
-							<span className='sr-only'>Open menu</span>
-							<FaBars />
-						</button>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className='bg-white py-2 border-b'>
+      <div className='container max-w-screen-xl mx-auto px-4'>
+        <div className='flex flex-wrap items-center'>
+          <div className='flex-shrink-0 mr-5'>
+            <a href='/'>
+              <Image
+                src={logoImg.src}
+                height='40'
+                width='120'
+                alt='GoShopping'
+              />
+            </a>
+          </div>
+          <Search />
+
+          <div className='flex items-center space-x-2 ml-auto'>
+            <Link
+              href='/cart'
+              className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
+            >
+              <BsCart className='inline lg:hidden ml-1' />
+              <span className='hidden lg:inline ml-1'>Cart</span> (
+              <b>{cartItems?.length || 0}</b>)
+            </Link>
+            <Link
+              href='/login'
+              className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
+            >
+              <BsFillPersonFill className='inline lg:hidden ml-1' />
+              <span className='hidden lg:inline ml-1'>Sign in</span>
+            </Link>
+            <Link href='/me'>
+              <div className='flex items-center mb-4 space-x-3 mt-4 cursor-pointer'>
+                <img
+                  className='w-10 h-10 rounded-full'
+                  src={profileImg.src}
+                  loading='lazy'
+                />
+                <div className='space-y-1 font-medium'>
+                  <p>
+                    testuser
+                    <time className='block text-sm text-gray-500 dark:text-gray-400'>
+                      test@gmail.com
+                    </time>
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className='lg:hidden ml-2'>
+            <button
+              type='button'
+              className='bg-white p-3 inline-flex items-center rounded-md text-black hover:bg-gray-200 hover:text-gray-800 border border-transparent'
+            >
+              <span className='sr-only'>Open menu</span>
+              <FaBars />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
