b8df222fb1951146af41b80b8d2842e79a74cb06
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-07 01:18:23 +0600

    products displayed based on filter

--- components/products/ProductLists.jsx
+++ components/products/ProductLists.jsx
@@ -8,57 +8,58 @@ import CustomPagination from './CustomPagination';
 import queryString from 'query-string';
 
 const ProductLists = ({ searchParams }) => {
-	const [productsData, setProductsData] = useState([]);
-	const [productsDataPerPage, setProductsDataPerPage] = useState(0);
-	const [productsDataCount, setProductsDataCount] = useState(0);
+  const [productsData, setProductsData] = useState([]);
+  const [productsDataPerPage, setProductsDataPerPage] = useState(0);
+  const [productsDataCount, setProductsDataCount] = useState(0);
 
-	// console.log(process.env.API_URL);
-	const getProducts = async (searchParams) => {
-		const urlParams = {
-			keyword: searchParams?.keyword,
-			page: searchParams?.page,
-		};
+  // console.log(process.env.API_URL);
+  const getProducts = async (searchParams) => {
+    const urlParams = {
+      keyword: searchParams?.keyword,
+      page: searchParams?.page,
+      category: searchParams?.category,
+      'price[gte]': searchParams?.min,
+      'price[lte]': searchParams?.max,
+      'ratings[gte]': searchParams?.ratings,
+    };
 
-		const searchQuery = queryString.stringify(urlParams);
+    const searchQuery = queryString.stringify(urlParams);
 
-		// const { data } = await axios.get(`${process.env.API_URL}/api/v1/products?${searchQuery}`);
-		const { data } = await axios.get(`/api/v1/products?${searchQuery}`);
-		// console.log('data', data?.data);
-		setProductsData(data?.data);
-		setProductsDataPerPage(data?.resPerPage);
-		setProductsDataCount(data?.filteredProductsCount);
+    // const { data } = await axios.get(`${process.env.API_URL}/api/v1/products?${searchQuery}`);
+    const { data } = await axios.get(`/api/v1/products?${searchQuery}`);
+    // console.log('data', data?.data);
+    setProductsData(data?.data);
+    setProductsDataPerPage(data?.resPerPage);
+    setProductsDataCount(data?.filteredProductsCount);
 
-		return data?.data;
-	};
+    return data?.data;
+  };
 
-	useEffect(() => {
-		getProducts(searchParams);
-	}, [searchParams]);
-	console.log('products data', productsData);
+  useEffect(() => {
+    getProducts(searchParams);
+  }, [searchParams]);
+  console.log('products data', productsData);
 
-	return (
-		<section className='py-12'>
-			<div className='container max-w-screen-xl mx-auto px-4'>
-				<div className='flex flex-col md:flex-row -mx-4'>
-					<Filters />
+  return (
+    <section className='py-12'>
+      <div className='container max-w-screen-xl mx-auto px-4'>
+        <div className='flex flex-col md:flex-row -mx-4'>
+          <Filters />
 
-					<main className='md:w-2/3 lg:w-3/4 px-3'>
-						{productsData?.map((product) => (
-							<ProductItem
-								key={product?._id}
-								product={product}
-							/>
-						))}
+          <main className='md:w-2/3 lg:w-3/4 px-3'>
+            {productsData?.map((product) => (
+              <ProductItem key={product?._id} product={product} />
+            ))}
 
-						<CustomPagination
-							resPerPage={productsDataPerPage}
-							productsCount={productsDataCount}
-						/>
-					</main>
-				</div>
-			</div>
-		</section>
-	);
+            <CustomPagination
+              resPerPage={productsDataPerPage}
+              productsCount={productsDataCount}
+            />
+          </main>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductLists;
