79b4de81b2602b369abfde7648453096eb9bbad2
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-07 18:29:40 +0600

    auth controller for register user

---
+++ backend/controllers/authController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: user,
+      message: 'User created successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
+};
