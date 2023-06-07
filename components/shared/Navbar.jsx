b1fa24233b63371b29856a0eecae55d3386a22eb
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-07 22:01:05 +0600

    navbar remodified with user context

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import logoImg from '../../assets/images/logo.png';
@@ -9,10 +9,23 @@ import Search from './Search';
 import { BsCart, BsFillPersonFill } from 'react-icons/bs';
 import { FaBars } from 'react-icons/fa';
 import CartContext from '@/context/CartContext';
+import AuthContext from '@/context/AuthContext';
+import { useSession } from 'next-auth/react';
 
 const Navbar = () => {
+  const { data: session } = useSession();
   const { cart } = useContext(CartContext);
+  const { user, setUser } = useContext(AuthContext);
+
   const cartItems = cart?.cartItems;
+
+  console.log('data session', session);
+
+  useEffect(() => {
+    if (session) {
+      setUser(session?.user);
+    }
+  }, [session, setUser]);
 
   return (
     <header className='bg-white py-2 border-b'>
@@ -39,30 +52,34 @@ const Navbar = () => {
               <span className='hidden lg:inline ml-1'>Cart</span> (
               <b>{cartItems?.length || 0}</b>)
             </Link>
-            <Link
-              href='/sign-in'
-              className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
-            >
-              <BsFillPersonFill className='inline lg:hidden ml-1' />
-              <span className='hidden lg:inline ml-1'>Sign in</span>
-            </Link>
-            <Link href='/me'>
-              <div className='flex items-center mb-4 space-x-3 mt-4 cursor-pointer'>
-                <img
-                  className='w-10 h-10 rounded-full'
-                  src={profileImg.src}
-                  loading='lazy'
-                />
-                <div className='space-y-1 font-medium'>
-                  <p>
-                    testuser
-                    <time className='block text-sm text-gray-500 dark:text-gray-400'>
-                      test@gmail.com
-                    </time>
-                  </p>
+
+            {!user ? (
+              <Link
+                href='/sign-in'
+                className='px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300'
+              >
+                <BsFillPersonFill className='inline lg:hidden ml-1' />
+                <span className='hidden lg:inline ml-1'>Sign in</span>
+              </Link>
+            ) : (
+              <Link href='/me'>
+                <div className='flex items-center mb-4 space-x-3 mt-4 cursor-pointer'>
+                  <img
+                    className='w-10 h-10 rounded-full'
+                    src={profileImg.src}
+                    loading='lazy'
+                  />
+                  <div className='space-y-1 font-medium'>
+                    <p>
+                      {user?.name}
+                      <time className='block text-sm text-gray-500 dark:text-gray-400'>
+                        {user?.email}
+                      </time>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            )}
           </div>
           <div className='lg:hidden ml-2'>
             <button
