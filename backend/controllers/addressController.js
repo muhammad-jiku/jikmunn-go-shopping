e7e6c4dcdee004787404e842a403af1e1f09939d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-08 11:00:53 +0600

    get address controller

--- backend/controllers/addressController.js
+++ backend/controllers/addressController.js
@@ -17,3 +17,21 @@ export const newAddress = async (req, res) => {
     });
   }
 };
+
+export const getAddresses = async (req, res) => {
+  try {
+    const addresses = await Address.find({});
+
+    return res.status(200).json({
+      success: true,
+      data: addresses,
+      message: 'addresses displayed successfully!',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
+};
