d238161af1ad7e63e88f4b36989558792670000c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-08 10:50:50 +0600

    user address data fetching func added

--- components/dashboard/Profile.jsx
+++ components/dashboard/Profile.jsx
@@ -1,13 +1,30 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Link from 'next/link';
 import AuthContext from '@/context/AuthContext';
 import profileImg from '../../assets/images/default_profile_avatar.png';
 import { BsPlus } from 'react-icons/bs';
+import UserAddresses from './user/UserAddresses';
+import axios from 'axios';
 
 const Profile = () => {
+  const [addresses, setAddresses] = useState([]);
   const { user } = useContext(AuthContext);
+
+  const getAddresses = async () => {
+    // const { data } = await axios.get(`${process.env.API_URL}/api/v1/address`);
+    const { data } = await axios.get(`/api/v1/address`);
+
+    setAddresses(data?.data);
+
+    return data?.data;
+  };
+
+  useEffect(() => {
+    getAddresses();
+  }, []);
+  console.log('address data', addresses);
 
   return (
     <>
@@ -29,6 +46,8 @@ const Profile = () => {
       </figure>
 
       <hr className='my-4' />
+
+      <UserAddresses addresses={addresses} />
 
       <Link href='/dashboard/user/address'>
         <button className='px-4 py-2 inline-block text-blue-600 border border-gray-300 rounded-md hover:bg-gray-100'>
