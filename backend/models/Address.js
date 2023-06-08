3108441f72bab0fe314fd082f9c83e1601b3909a
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-08 11:04:46 +0600

    address and product model modification

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -26,6 +26,11 @@ const addressSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     createdAt: {
       type: Date,
       default: Date.now,
