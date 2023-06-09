531e58c6bb88041efeffa290af488816ef83366c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-09 10:08:45 +0600

    update user profile controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,3 +23,38 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+    const newUserData = await {
+      name: req.body.name,
+      email: req.body.email,
+    };
+
+    if (req.files.length > 0) {
+      const uploader = async (path) =>
+        await uploads(path, 'jikmunn-go-shopping/avatars');
+
+      const file = req.files[0];
+      const { path } = file;
+
+      const avatarResponse = await uploader(path);
+      fs.unlinkSync(path);
+      newUserData.avatar = avatarResponse;
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, newUserData);
+
+    return res.status(200).json({
+      success: true,
+      data: user,
+      message: 'User updated successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
+};
