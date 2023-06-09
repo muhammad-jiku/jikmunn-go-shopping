f1ffdcf3dfed181ce14ed126ca0e5176536f4691
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-09 10:09:56 +0600

    product controller necessary modification

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,77 +1,77 @@
 import Product from '../models/Product';
 import APIFilters from '../utils/ApiFilters';
 
-export const newProduct = async (req, res, next) => {
-	try {
-		const product = await Product.create(req.body);
+export const newProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
 
-		return res.status(201).json({
-			success: true,
-			data: product,
-			message: 'Product added successfully',
-		});
-	} catch (error) {
-		console.log(error);
-		return res.status(500).json({
-			success: false,
-			message: 'Something went wrong',
-		});
-	}
+    return res.status(201).json({
+      success: true,
+      data: product,
+      message: 'Product added successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
 };
 
-export const getProducts = async (req, res, next) => {
-	try {
-		const resPerPage = 4;
-		const productsCount = await Product.countDocuments();
+export const getProducts = async (req, res) => {
+  try {
+    const resPerPage = 4;
+    const productsCount = await Product.countDocuments();
 
-		const apiFilters = new APIFilters(Product.find({}), req.query)
-			.search()
-			.filter();
+    const apiFilters = new APIFilters(Product.find({}), req.query)
+      .search()
+      .filter();
 
-		let products = await apiFilters.query;
-		const filteredProductsCount = products.length;
+    let products = await apiFilters.query;
+    const filteredProductsCount = products.length;
 
-		apiFilters.pagination(resPerPage);
+    apiFilters.pagination(resPerPage);
 
-		products = await apiFilters.query.clone();
+    products = await apiFilters.query.clone();
 
-		return res.status(200).json({
-			success: true,
-			productsCount,
-			resPerPage,
-			filteredProductsCount,
-			data: products,
-			message: 'products displayed successfully!',
-		});
-	} catch (error) {
-		console.log(error);
-		return res.status(500).json({
-			success: false,
-			message: 'Something went wrong',
-		});
-	}
+    return res.status(200).json({
+      success: true,
+      productsCount,
+      resPerPage,
+      filteredProductsCount,
+      data: products,
+      message: 'products displayed successfully!',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
 };
 
-export const getProduct = async (req, res, next) => {
-	try {
-		const product = await Product.findById({ _id: req.query.id });
+export const getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById({ _id: req.query.id });
 
-		if (!product) {
-			res.status(404).json({
-				error: 'Something went missing!',
-			});
-		}
+    if (!product) {
+      res.status(404).json({
+        error: 'Something went missing!',
+      });
+    }
 
-		return res.status(200).json({
-			success: true,
-			data: product,
-			message: 'product displayed successfully!',
-		});
-	} catch (error) {
-		console.log(error);
-		return res.status(500).json({
-			success: false,
-			message: 'Something went wrong',
-		});
-	}
+    return res.status(200).json({
+      success: true,
+      data: product,
+      message: 'product displayed successfully!',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
 };
