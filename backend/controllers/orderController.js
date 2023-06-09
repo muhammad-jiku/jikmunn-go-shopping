ad44091d2e0112fe545a1c2f7e199005958380ec
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-09 23:21:54 +0600

    order webhook controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
+import Order from '../models/Order';
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
+import getRawBody from 'raw-body';
 
 export const checkoutSession = async (req, res) => {
   try {
@@ -45,5 +47,77 @@ export const checkoutSession = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Something went wrong!' });
+  }
+};
+
+async function getCartItems(line_items) {
+  return new Promise((resolve, reject) => {
+    let cartItems = [];
+
+    line_items?.data?.forEach(async (item) => {
+      const product = await stripe.products.retrieve(item.price.product);
+      const productId = product.metadata.productId;
+
+      cartItems.push({
+        product: productId,
+        name: product.name,
+        price: item.price.unit_amount_decimal / 100,
+        quantity: item.quantity,
+        image: product.images[0],
+      });
+
+      if (cartItems.length === line_items?.data.length) {
+        resolve(cartItems);
+      }
+    });
+  });
+}
+
+export const webhook = async (req, res) => {
+  try {
+    const rawBody = await getRawBody(req);
+    const signature = req.headers['stripe-signature'];
+
+    const event = stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+
+      const line_items = await stripe.checkout.sessions.listLineItems(
+        event.data.object.id
+      );
+
+      const orderItems = await getCartItems(line_items);
+      const userId = session.client_reference_id;
+      const amountPaid = session.amount_total / 100;
+
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+        amountPaid,
+        taxPaid: session.total_details.amount_tax / 100,
+      };
+
+      const orderData = {
+        user: userId,
+        shippingInfo: session.metadata.shippingInfo,
+        paymentInfo,
+        orderItems,
+      };
+
+      const order = await Order.create(orderData);
+
+      res.status(201).json({
+        success: true,
+        data: order,
+        message: 'Order created successfully!',
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
