ef79c0033cea0e0e1ea4a905d846a853bb636c99
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-09 23:11:42 +0600

    checkout session order controller func modification

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,19 +1,49 @@
-export const checkoutSession = async (req, res) => {
-  const body = await req.body;
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
-  const line_items = body?.items?.map((item) => {
-    return {
-      price_data: {
-        currency: 'usd',
-        product_data: {
-          name: item.name,
-          images: [item.image],
-          metadata: { productId: item.product },
+export const checkoutSession = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const line_items = body?.items?.map((item) => {
+      return {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.name,
+            images: [item.image],
+            metadata: { productId: item.product },
+          },
+          unit_amount: item.price * 100,
         },
-        unit_amount: item.price * 100,
-      },
-      tax_rates: ['txr_1MUVJSAlHMiRMt8E2khIxJEi'],
-      quantity: item.quantity,
-    };
-  });
+        tax_rates: ['txr_1MUVJSAlHMiRMt8E2khIxJEi'],
+        quantity: item.quantity,
+      };
+    });
+
+    const shippingInfo = body?.shippingInfo;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      success_url: `${process.env.API_URL}/me/orders?order_success=true`,
+      cancel_url: `${process.env.API_URL}`,
+      customer_email: req?.user?.email,
+      client_reference_id: req?.user?._id,
+      mode: 'payment',
+      metadata: { shippingInfo },
+      shipping_options: [
+        {
+          shipping_rate: 'shr_1MUVKxAlHMiRMt8EmUp4SKxz',
+        },
+      ],
+      line_items,
+    });
+
+    res.status(200).json({
+      url: session.url,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Something went wrong!' });
+  }
 };
