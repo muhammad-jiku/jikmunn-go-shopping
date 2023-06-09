7809c46d30547b965ad11e70385b8395abf889f0
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-10 01:18:38 +0600

    display users orders api

---
+++ pages/api/v1/orders/me/index.js
@@ -0,0 +1,28 @@
+import { myOrders } from '@/backend/controllers/orderController';
+import { isAuthenticatedUser } from '@/backend/middlewares/auth';
+import ErrorHandlingChecker from '@/backend/middlewares/errors';
+import { connectToDB } from '@/backend/utils/dbConnection';
+import nc from 'next-connect';
+
+// export default async function handler(req, res) {
+//   if (req.method === 'GET') {
+//     await connectToDB();
+//     await isAuthenticatedUser(req, res, () => {
+//       myOrders(req, res);
+//     });
+//   } else {
+//     return res.status(405).json({
+//       message: 'Method not allowed! Please try again',
+//     });
+//   }
+// }
+
+const handler = nc({
+  onError: ErrorHandlingChecker,
+});
+
+connectToDB();
+
+handler.use(isAuthenticatedUser).get(myOrders);
+
+export default handler;
