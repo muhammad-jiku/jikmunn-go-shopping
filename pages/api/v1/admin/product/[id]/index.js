66fa525ec91ea75642b85a3ef20c1b0a28da51a9
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-10 13:19:22 +0600

    admin delete product api

--- pages/api/v1/admin/product/[id]/index.js
+++ pages/api/v1/admin/product/[id]/index.js
@@ -1,4 +1,7 @@
-import { updateProduct } from '@/backend/controllers/productController';
+import {
+  updateProduct,
+  deleteProduct,
+} from '@/backend/controllers/productController';
 import {
   authorizeRoles,
   isAuthenticatedUser,
@@ -8,11 +11,17 @@ import { connectToDB } from '@/backend/utils/dbConnection';
 import nc from 'next-connect';
 
 // export default async function handler(req, res) {
-//   if (req.method === 'POST') {
+//   if (req.method === 'PUT') {
 //     await connectToDB();
 //     await isAuthenticatedUser(req, res, () => {
 //       authorizeRoles('admin');
 //       updateProduct(req, res);
+//     });
+//   } else if (req.method === 'DELETE') {
+//     await connectToDB();
+//     await isAuthenticatedUser(req, res, () => {
+//       authorizeRoles('admin');
+//       deleteProduct(req, res);
 //     });
 //   } else {
 //     return res.status(405).json({
@@ -28,5 +37,6 @@ const handler = nc({
 connectToDB();
 
 handler.use(isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct);
+handler.use(isAuthenticatedUser, authorizeRoles('admin')).delete(deleteProduct);
 
 export default handler;
