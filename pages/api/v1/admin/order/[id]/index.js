fdac8484da0948ca8d233cf3cbe6fab7fb752d65
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-10 14:42:54 +0600

    admin user order api

---
+++ pages/api/v1/admin/order/[id]/index.js
@@ -0,0 +1,32 @@
+import { getOrder } from '@/backend/controllers/orderController';
+import {
+  authorizeRoles,
+  isAuthenticatedUser,
+} from '@/backend/middlewares/auth';
+import ErrorHandlingChecker from '@/backend/middlewares/errors';
+import { connectToDB } from '@/backend/utils/dbConnection';
+import nc from 'next-connect';
+
+// export default async function handler(req, res) {
+//   if (req.method === 'GET') {
+//     await connectToDB();
+//     await isAuthenticatedUser(req, res, () => {
+//       authorizeRoles('admin');
+//       getOrder(req, res);
+//     });
+//   } else {
+//     return res.status(405).json({
+//       message: 'Method not allowed! Please try again',
+//     });
+//   }
+// }
+
+const handler = nc({
+  onError: ErrorHandlingChecker,
+});
+
+connectToDB();
+
+handler.use(isAuthenticatedUser, authorizeRoles('admin')).get(getOrder);
+
+export default handler;
