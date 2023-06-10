59eb325cc12a37fd92e7923d56897a313806da0f
muhammad-jiku <muhammadjiku364@gmail.com>
2023-06-10 11:34:08 +0600

    product provider added

--- app/provider/GlobalProvider.jsx
+++ app/provider/GlobalProvider.jsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from 'next-auth/react';
 import { AuthProvider } from '@/context/AuthContext';
 import { CartProvider } from '@/context/CartContext';
+import { ProductProvider } from '@/context/ProductContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,7 +13,9 @@ export function GlobalProvider({ children }) {
       <ToastContainer position='bottom-right' />
       <AuthProvider>
         <CartProvider>
-          <SessionProvider>{children}</SessionProvider>
+          <ProductProvider>
+            <SessionProvider>{children}</SessionProvider>
+          </ProductProvider>
         </CartProvider>
       </AuthProvider>
     </>
